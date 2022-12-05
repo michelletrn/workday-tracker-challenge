2942282a4e2cfb5c58c9ccc1003568f2819940e2
michelle <113802263+michelletrn@users.noreply.github.com>
2022-12-05 17:32:30 -0500

    date display and hours are displaying, issue with 9AM, it is not applied the correct style

--- assets/script.js
+++ assets/script.js
@@ -14,27 +14,58 @@
 
 var dateTime = $('#currentDay');
 var saveBtns = $('.saveBtn');//all of the save btns
-var hourBlocks = $('.container-lg');//container that holds each hour's id
 
-var nowhr = dayjs().format('hh'); // just the current hr
+var hoursContainer = $('.container-lg');//container that holds each hour's id
+var hourBlock = $('.time-block'); //individual hour blocks
+var hourToDo = $('.description'); //each hour block's text box
+var past = $('.past');
+var present = $('.present');
+var future = $('.future');
+var hourNum = $('.hour'); //same line as the time number
+
+//each hour block's unique id, incase
+var nine = $('#hour-9');
+var ten = $('#hour-10');
+var eleven = $('#hour-11');
+var twelve = $('#hour-12');
+var one = $('#hour-1');
+var two = $('#hour-2');
+var three = $('#hour-3');
+var four = $('#hour-4');
+var five = $('#hour-5');
 
 
+var nowhr = dayjs().format('hA'); // just the current hr + AM, will be using this var to compare
+// console.log(nowhr);
 function dateTimeDisplay() {
-  var now = dayjs().format("dddd, MMM DD, hh:mm a");
+  var now = dayjs().format("dddd, MMMM DD");
   dateTime.text(now);
+  checkHour();
 }
 
+//why is 9 am not classing correctly, but if i change 9AM to 10AM, it applies correctly; also added an 8am and it was assigned the same class as 9am
+function checkHour() {
+  for(var i = 0; i < hourNum.length; i++) {
+    if (nowhr === hourNum[i].innerHTML) {
+      $(hourNum[i]).parent().addClass("present");
+    }
+    if (nowhr > hourNum[i].innerHTML) {
+      $(hourNum[i]).parent().addClass("past");
+    } 
+    else if (nowhr < hourNum[i].innerHTML) {
+      $(hourNum[i]).parent().addClass("future"); 
+    }
+    console.log(nowhr, hourNum[i].innerHTML);
+    console.log(nowhr > hourNum[i].innerHTML);
+  }
+}
+
+
+
+
+
+
 dateTimeDisplay();
-
-
-
-
-
-
-for (var i = 0; i < saveBtns.length; i++) {
-  saveBtns[i].on('click', working());
-};
-
 
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
